313e245df17a28b3949f48ae0a65b90bdab32b6c
claudioantonio <claudio.engdist@gmail.com>
2020-07-27 23:45:50 -0300

    Código base preparado durante a vídeo-aula

---
+++ src/components/Menu/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from '../Button';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+              <img className="Logo" src={Logo} alt="ReactFlix"></img>
+            </a>
+
+            <Button className="ButtonLink" href="/">Novo Link</Button>
+        </nav>
+    )
+}
+
+export default Menu;
