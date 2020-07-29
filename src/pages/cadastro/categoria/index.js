fd9ed4fefa51d927f14ac67abfe5cb7519122df3
claudioantonio <claudio.engdist@gmail.com>
2020-07-28 23:44:15 -0300

    Inclusão do form para categorias de video

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import PageDefault from '../../templatepage/PageDefault';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../pages/templatepage/PageDefault';
 
 function CadastroCategoria() {
-    return(
-      <PageDefault>
-        <h1>Cadastro de Categoria</h1>
-        <Link to="/">
-          Voltar home
-        </Link>
-      </PageDefault>
-    );
-  }
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
+
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
+}
 
 export default CadastroCategoria;
